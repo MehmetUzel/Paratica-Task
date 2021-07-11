bdd2b9921eb6259e736035acee9a26bec63f6dab
Mehmet Uzel <98mehmetuzel@gmail.com>
2021-07-11 20:21:05 +0300

    Removed experimental codes and added searching ability by name
    Removed experimental codes and added searching ability by name includes case insensitive way

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 
 from flask import Flask, jsonify, request, render_template, Response
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 app = Flask(__name__)
 
@@ -46,55 +47,11 @@ class UserAction(db.Model):
         self.timestamp = timestamp
 
 
-sampleData = [
-    {
-        "name": "Molecule Man",
-        "index": 0,
-        "age": 29,
-        "secretIdentity": "Dan Jukes",
-        "powers": [
-            "Radiation resistance",
-            "Turning tiny",
-            "Radiation blast"
-        ]
-    },
-    {
-        "name": "Madame Uppercut",
-        "age": 39,
-        "index": 1,
-        "secretIdentity": "Jane Wilson",
-        "powers": [
-            "Million tonne punch",
-            "Damage resistance",
-            "Superhuman reflexes"
-        ]
-    },
-    {
-        "name": "Eternal Flame",
-        "age": 1000000,
-        "index": 2,
-        "secretIdentity": "Unknown",
-        "powers": [
-            "Immortality",
-            "Heat Immunity",
-            "Inferno",
-            "Teleportation",
-            "Interdimensional travel"
-        ]
-    }
-]
-
-
 @app.route('/users', methods=['GET'])
 def index():
     users = db.session.query(UserModel)
     result = [{'name': getattr(d, 'name')} for d in users]
     return jsonify(users=result)
-
-
-@app.route('/sample', methods=['GET'])
-def get():
-    return jsonify({'Users': sampleData})
 
 
 @app.route('/users/<int:id>', methods=['GET'])
@@ -104,37 +61,13 @@ def get_name(id):
 
 
 # simple call from terminal " curl -i "Content-Type: App;ication/json" -X POST http://127.0.0.1:5000/sample "
-@app.route('/sample', methods=['POST'])
+@app.route('/search', methods=['POST'])
 def add_user():
-    user = {
-        "name": "Mehmet Uzel",
-        "age": 999999999,
-        "index": 3,
-        "secretIdentity": "Ironman",
-        "powers": [
-            "Code Bending",
-            "Writing code without bugs",
-            "Googles like a god",
-            "Mind Reading",
-            "Best CS GO Player"
-        ]
-    }
-    sampleData.append(user)
-    return jsonify({'Created': sampleData})
+    user = db.session.query(UserModel).filter(UserModel.name.ilike('%Ana%'))
+    #user = db.session.query(UserModel).filter(func.lower(UserModel.name) == func.lower('zoe Abramson'))
+    result = [{'id': getattr(d,'id'),'name': getattr(d, 'name')} for d in user]
+    return jsonify(users=result)
 
-
-# simple call from terminal " curl -i "Content-Type: App;ication/json" -X PUT http://127.0.0.1:5000/sample/0 "
-@app.route("/sample/<int:index_change>", methods=['PUT'])
-def user_update(index_change):
-    sampleData[index_change]['secretIdentity'] = "Changed!"
-    return jsonify({'user': sampleData[index_change]})
-
-
-# simple call from terminal " curl -i "Content-Type: App;ication/json" -X DELETE http://127.0.0.1:5000/sample/0 "
-@app.route("/sample/<int:index_delete>", methods=['DELETE'])
-def user_delete(index_delete):
-    sampleData.remove(sampleData[index_delete])
-    return jsonify({'result': True})
 
 
 if __name__ == "__main__":
